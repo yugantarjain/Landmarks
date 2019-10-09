17066078f2fdbef1749c1751650e64b69920f7f8
Yugantar Jain <yugantar8jain@gmail.com>
2019-10-09 13:33:36 +0530

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Yugantar Jain on 09/10/19.
+//  Copyright © 2019 Yugantar Jain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
