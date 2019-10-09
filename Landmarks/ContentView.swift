734adfeeab71896c80901d77ec9afca3fcec0ebb
Yugantar Jain <yugantar8jain@gmail.com>
2019-10-09 13:44:30 +0530

    stack and leading alignment done.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            Text("Joshua Tree National Park")
+                .font(.subheadline)
+        }
             
     }
 }
